4fe2d4e0e6664723d078ad819f051625b7f373bb
Sergey_Yatsenko <sergey_y@inbox.ru>
2023-06-04 16:36:50 +0300

    code moved to def draw

--- main.py
+++ main.py
@@ -28,30 +28,10 @@ import asyncio
 #         time.sleep(0.3)
 
 
-async def draw(canvas, symbol='*'):
-    while True:
-        curses.curs_set(False)
-        row, column = (5, 20)
-
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(2)
-
-        canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0.3)
-
-        canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0.5)
-
-        canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0.3)
-
-        time.sleep(10)
-
-
-if __name__ == '__main__':
-
+def draw(row=1, column=1):
     curses.update_lines_cols()
-    coroutine = curses.wrapper(draw)
+    coroutine = curses.wrapper(blink(row=row, column=column))
+
     print(coroutine)
     print(type(coroutine))
     print(dir(coroutine))
@@ -61,3 +41,22 @@ if __name__ == '__main__':
             coroutine.send(None)
         except StopIteration:
             break
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+
+if __name__ == '__main__':
+    draw(5, 22)
