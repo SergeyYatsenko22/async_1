7777286b1b59ed66ab726224967f18d1519dfee8
Sergey_Yatsenko <sergey_y@inbox.ru>
2023-06-09 16:59:08 +0300

    Added gunshot

--- main.py
+++ main.py
@@ -4,6 +4,36 @@ import asyncio
 import random
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 # def draw(canvas):
 #     while True:
 #         curses.curs_set(False)
@@ -49,7 +79,19 @@ async def blink(canvas, row, column, symbol="*"):
 
 
 def draw(canvas):
-    print(curses.window.getmaxyx(canvas)[0])
+    coroutine_gun = fire(canvas, 14, 30)
+
+    while True:
+        try:
+            curses.curs_set(False)
+            coroutine_gun.send(None)
+            time.sleep(0.1)
+            canvas.refresh()
+        except StopIteration:
+            break
+    time.sleep(3)
+
+    # print(curses.window.getmaxyx(canvas)[0])
     star_field = []
     coroutines = []
     star_count = 0
@@ -65,14 +107,6 @@ def draw(canvas):
             coroutines.append(blink(canvas=canvas, row=row_index, column=column_index, symbol=star_symbol))
         else:
             continue
-
-    # print (coroutines)
-
-    # time.sleep(100)
-
-    # print(coroutine)
-    # print(type(coroutine))
-    # print(dir(coroutine))
 
     while True:
         curses.curs_set(False)
